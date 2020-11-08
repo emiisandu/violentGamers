7003a20978313ff0fd3ac124a353937a6fd2a77d
miv42 <62116681+miv42@users.noreply.github.com>
2020-11-08 21:08:36 +0200

    registration-page prototype
    Added a registration page prototype and an attempt at routing it

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+import {RegistrationComponent} from "./registration/registration.component";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'registration', component:RegistrationComponent, pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: false} )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
