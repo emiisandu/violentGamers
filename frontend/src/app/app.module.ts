ea55709f1a40a2a87b429665fb94c69443b71486
miv42 <62116681+miv42@users.noreply.github.com>
2020-11-16 11:18:02 +0200

    form validation and data post
    added form validation and function to post form data on submit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -12,10 +14,13 @@ import { HomeComponent } from './home/home.component';
     RegistrationComponent,
     HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,12 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../shared/user.model";
+import {NgForm} from '@angular/forms';
+import {RegisterService} from './register.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
 
     title = 'registration';
+
+    user: User;
+    registerForm;
+    emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+    constructor(
+      private registerService: RegisterService,
+      private formBuilder: FormBuilder,
+      ) {
+      this.registerForm = this.formBuilder.group({
+        Username: '',
+        Password: '',
+        Email: '',
+        Lastname: '',
+        Firstname:''
+      });
+      this.user = new User();
+    }
+
+    ngOnInit(){
+      this.resetForm();
+    }
+
+    resetForm(form?:NgForm){
+      if(form != null){
+        form.reset();
+        this.user ={
+          UserName: '',
+          Password: '',
+          Email:'',
+          Firstname: '',
+          Lastname: ''
+        }
+      }
+    }
+
+    submitForm(customerData){
+
+      this.registerService.callPost(this.user).subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      );
+
+    }
 
 }
