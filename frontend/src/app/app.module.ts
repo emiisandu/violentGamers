8e8685d75cf731ef86ff483aa06bb73f88b0a2d7
soniapoj <62116582+soniapoj@users.noreply.github.com>
2020-11-16 13:56:58 +0200

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import {FormsModule} from "@angular/forms";
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
