58bce4b2029fe0af8bb787f672cf527099222727
Ovidiu <ovi_muntean99@yahoo.com>
2020-11-21 20:42:14 +0200

    Added User Project History and User Project History Repository

---
+++ backend/proiectcolectiv/src/main/java/com/ubb/proiectcolectiv/UserProjectHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.ubb.proiectcolectiv;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProjectHistoryRepository extends JpaRepository<User, Integer> {
+
+}
