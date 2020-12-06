1b45f10a2ec3bba326950b48c425a926053b094c
Theophania <49263937+Theophania@users.noreply.github.com>
2020-12-06 23:09:45 +0200

    new skill page

--- frontend/src/app/user-skills/user-skills.component.ts
+++ frontend/src/app/user-skills/user-skills.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {FormControl, Validators} from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 
 interface Category {
   value: string;
