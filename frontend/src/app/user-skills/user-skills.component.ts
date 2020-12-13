06eab2b039dea7c61fdf6edff4a4e7336a9a1e63
miv42 <62116681+miv42@users.noreply.github.com>
2020-12-13 17:09:51 +0200

    userskills page tweaks

--- frontend/src/app/user-skills/user-skills.component.ts
+++ frontend/src/app/user-skills/user-skills.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-skills',
@@ -12,11 +13,28 @@ export class UserSkillsComponent implements OnInit {
   selectedValue: String;
   personalSkill: String;
 
-  constructor( ) { }
+  constructor( private router:Router ) { }
 
   ngOnInit(): void {
   }
 
 
+  // submits only the value from personalSkill to the Database
+  submitPersonalSkill(){
+    if(this.personalSkill.length > 0){
+      this.router.navigate(['/UserProfile']);
+    }
+  }
+
+  // submits all skills in the selected Value
+  submitChosenSkills(){
+    if(this.selectedValue.length >= 1){
+      // send each skill in the list to the Database
+      for(let skill in this.selectedValue){
+
+      }
+      this.router.navigate(['/UserProfile']);
+    }
+  }
 
 }
